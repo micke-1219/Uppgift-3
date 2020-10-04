ef4f7467b4f280f682b8c3bb626a9795b1f5d0c7
Mikael Gidmo <micke_1219@hotmail.com>
2020-10-04 13:39:01 +0200

    Add project files.

---
+++ IotConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Devices.Client;
+using SharedLibrary.Models;
+using SharedLibrary.Services;
+using System;
+
+namespace IotConsoleApp
+{
+    class Program
+    {
+        private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString("HostName=micke-1219.azure-devices.net;DeviceId=IotConsoleApp;SharedAccessKey=0axcU1TXYGUu+c3Y8aeiV9851uPYwVO7/DD3GWeHaCs=", TransportType.Mqtt);
+        static void Main(string[] args)
+        {
+            DeviceService.SendMessageAsync(deviceClient).GetAwaiter();
+            DeviceService.ReceiveMessageAsync(deviceClient).GetAwaiter();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SharedLibrary/Services/DeviceService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using SharedLibrary.Models;
+
+namespace SharedLibrary.Services
+{
+    public static class DeviceService
+    {
+        public static Random rnd = new Random();
+        public static async Task SendMessageAsync(DeviceClient deviceClient)
+        {
+            while (true)
+            {
+                var json = JsonConvert.SerializeObject(new TemperatureModel(rnd.Next(10, 30), rnd.Next(0, 100)));
+                var payload = new Message(Encoding.UTF8.GetBytes(json));
+                await deviceClient.SendEventAsync(payload);
+                Console.WriteLine($"Message sent: {json}");
+                await Task.Delay(60000);
+            }
+        }
+        public static async Task ReceiveMessageAsync(DeviceClient deviceClient)
+        {
+            while (true)
+            {
+                var payload = await deviceClient.ReceiveAsync();
+                if(payload == null)
+                {
+                    continue;
+                }
+                Console.WriteLine($"Message received: {Encoding.UTF8.GetString(payload.GetBytes())}");
+                await deviceClient.CompleteAsync(payload);
+            }
+        }
+    }
+}
